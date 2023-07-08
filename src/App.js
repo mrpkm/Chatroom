836305789042a3be95e55d9fb294d7bd561a4daa
mrpkm <mrpkdmk01@gmail.com>
2023-07-08 18:58:15 +0530

    adding

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import firebase from './firebase';
+import { getDatabase, push, ref, set, onChildAdded } from 'firebase/database'
+
+function App() {
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
+  const [msg, setMsg] = useState("");
+
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+
+  const handleInputChange = () => {
+    const inputName = prompt('Please enter your name:');
+    setName(inputName);
+  };
+
+ 
+
+
+  // useEffect(() => {
+  //   onChildAdded(chatListRef, (data) => {
+  //     // const c = [...chats];
+  //     // c.push(data.val());
+  //     // setChats(c)
+  //     setChats(chats => [...chats, data.val()])
+  //     console.log(data.val());
+  //   });
+  // }, [])
+
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      name, message: msg
+    })
+    setMsg("");
+  }
+
+
+  return (
+    <div className="app">
+      {name ? null : <div className="setnamebtn">
+        <button className="setbtn" onClick={handleInputChange}>Set Name</button>
+      </div>}
+      {name ?
+        <div>
+          <h1>user: {name}</h1>
+          <div className="chat-container">
+            {chats.map((c, i) => <div key={i} className={`container ${c.name === name ? 'me' : ''}`}>
+              <p className="chatbox">
+                <strong>{c.message}</strong>
+                <span>{c.name}</span>
+              </p>
+            </div>)}
+
+          </div>
+          <div className="botton">
+            <input type="text" required onInput={e => setMsg(e.target.value)} placeholder="enter you chat.." value={msg} />
+            <button onClick={e => sendChat()}>send</button>
+          </div>
+        </div> : null}
+
+    </div >
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV4kid2I8CL-UgqlPy6Xa_XZNEFyGY3zI",
+  authDomain: "pk-chat-88c7c.firebaseapp.com",
+  projectId: "pk-chat-88c7c",
+  storageBucket: "pk-chat-88c7c.appspot.com",
+  messagingSenderId: "1013056331220",
+  appId: "1:1013056331220:web:d58f8a21ee5f61cc540a41"
+};
+
+export const app = initializeApp(firebaseConfig);
